5da2ecfbbf7ab9bdffa7484989189f8e71179ee2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-08-07 12:26:13 +0200

    feat(query): improve logging
    BREAKING CHANGE: this changes the API of athenaQuery

--- src/athenaQuery.ts
+++ src/athenaQuery.ts
@@ -1,77 +1,123 @@
 import { Athena } from 'aws-sdk'
-import { exponential } from 'backoff'
+import { exponential, Backoff } from 'backoff'
 
-const noop = () => undefined
+type logFn = (...args: any) => void
+const noLog = () => undefined
 
+/**
+ * Construct a query executor
+ * @param athena an AWS Athena instance
+ * @param WorkGroup the name of the Athena WorkGroup
+ * @param backoff an instance of Backoff, if none provided a default one will be used
+ * @param debugLog provide a method to receive debug logs
+ * @param errorLog provide a method to receive error logs
+ */
 export const athenaQuery = ({
 	athena,
 	WorkGroup,
-	logDebug,
+	backoff,
+	debugLog,
+	errorLog,
 }: {
 	athena: Athena
 	WorkGroup: string
-	logDebug?: (message: string) => void
-	skip?: number
-}) => async ({ QueryString }: { QueryString: string }) => {
-	const d = logDebug || noop
-	d(QueryString)
-	const { QueryExecutionId } = await athena
-		.startQueryExecution({
-			WorkGroup,
-			QueryString,
+	backoff?: Backoff
+	debugLog?: logFn | undefined
+	errorLog?: logFn | undefined
+}) =>
+	/**
+	 * Execute a query
+	 * @param QueryString the query string
+	 */
+	async ({
+		QueryString,
+	}: {
+		QueryString: string
+	}): Promise<Athena.ResultSet> => {
+		const d: logFn = debugLog || noLog
+		const e: logFn = errorLog || noLog
+		const b =
+			backoff ||
+			(() => {
+				const b = exponential({
+					randomisationFactor: 0,
+					initialDelay: 1000,
+					maxDelay: 5000,
+				})
+				b.failAfter(8) // 32000
+				return b
+			})()
+
+		d({ QueryString })
+		const { QueryExecutionId } = await athena
+			.startQueryExecution({
+				WorkGroup,
+				QueryString,
+			})
+			.promise()
+		if (!QueryExecutionId) {
+			throw new Error(`Query failed!`)
+		}
+		d({ QueryExecutionId })
+
+		await new Promise((resolve, reject) => {
+			b.on('ready', async () => {
+				const res = await athena
+					.getQueryExecution({ QueryExecutionId })
+					.promise()
+				const State =
+					(res.QueryExecution &&
+						res.QueryExecution.Status &&
+						res.QueryExecution.Status.State) ||
+					'unknown'
+
+				d({
+					QueryExecutionId,
+					State,
+				})
+
+				switch (State) {
+					case 'RUNNING':
+						b.backoff()
+						break
+					case 'FAILED':
+						console.error(res.QueryExecution)
+						e({
+							QueryExecutionId,
+							QueryExecution: res.QueryExecution,
+						})
+						reject(new Error(`Query ${QueryExecutionId} failed!`))
+						break
+					case 'SUCCEEDED':
+						resolve(res)
+						break
+					case 'unknown':
+					default:
+						e({
+							QueryExecutionId,
+							QueryExecution: res.QueryExecution,
+						})
+						reject(
+							new Error(`Query ${QueryExecutionId} has unexpected status!`),
+						)
+				}
+			})
+			b.on('fail', () => {
+				reject(new Error(`Timed out waiting for query ${QueryExecutionId}`))
+			})
+			b.backoff()
 		})
-		.promise()
-	if (!QueryExecutionId) {
-		throw new Error(`Query failed!`)
+
+		const { ResultSet } = await athena
+			.getQueryResults({ QueryExecutionId })
+			.promise()
+
+		if (!ResultSet || !ResultSet.Rows) {
+			e({
+				QueryExecutionId,
+				ResultSet,
+			})
+			throw new Error(`No resultset returned.`)
+		}
+		return ResultSet
 	}
-
-	await new Promise((resolve, reject) => {
-		const b = exponential({
-			randomisationFactor: 0,
-			initialDelay: 1000,
-			maxDelay: 5000,
-		})
-		b.failAfter(9) // 27750
-		b.on('ready', async () => {
-			const res = await athena.getQueryExecution({ QueryExecutionId }).promise()
-			const State =
-				(res.QueryExecution &&
-					res.QueryExecution.Status &&
-					res.QueryExecution.Status.State) ||
-				'unknown'
-
-			switch (State) {
-				case 'RUNNING':
-					console.debug(res)
-					b.backoff()
-					break
-				case 'FAILED':
-					console.error(res.QueryExecution)
-					reject(new Error(`Query ${QueryExecutionId} failed!`))
-					break
-				case 'SUCCEEDED':
-					resolve(res)
-					break
-				case 'unknown':
-					reject(new Error(`Query ${QueryExecutionId} has unknown status!`))
-					break
-				default:
-					console.error(res)
-					reject(new Error(`Query ${QueryExecutionId} has unexpected status!`))
-			}
-		})
-		b.on('fail', () => {
-			reject(new Error(`Timed out waiting for query ${QueryExecutionId}`))
-		})
-		b.backoff()
-	})
-
-	const { ResultSet } = await athena
-		.getQueryResults({ QueryExecutionId })
-		.promise()
-
-	if (!ResultSet || !ResultSet.Rows) {
-		throw new Error(`No resultset returned.`)
-	}
-	return ResultSet
-}
